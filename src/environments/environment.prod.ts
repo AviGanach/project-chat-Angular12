d85cd1704fc8ee1799588e6762a5165dae16ef98
AviGanach <ag0504122684@gmail.com>
2023-06-14 18:29:38 +0300

    Committing changes before updating Angular CLI

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCHt8xBen72iLOMy_FqIdtuxNvz5ms4Zhc",
+        authDomain: "project-chat-1c931.firebaseapp.com",
+        projectId: "project-chat-1c931",
+        storageBucket: "project-chat-1c931.appspot.com",
+        messagingSenderId: "130056561740",
+        appId: "1:130056561740:web:9e58188a29521b8ef540e9",
+        measurementId: "G-RCND2MTDEY"
+    }
+  };
+  
